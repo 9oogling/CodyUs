e5f67e0112ea2f1e8715ea4f0852346e42f684dc
KIM <wltn9169@naver.com>
2024-09-10 17:19:51 +0900

    ♻️refactor: 쿼리변경

--- src/main/java/com/team9oogling/codyus/domain/post/repository/PostRepository.java
+++ src/main/java/com/team9oogling/codyus/domain/post/repository/PostRepository.java
@@ -8,18 +8,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
-import java.util.List;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
     Page<Post> findByUser(User user, Pageable pageable);
 
     Page<Post> findAllByStatusAndCompletedDate(PostStatus status, LocalDate completedDate, Pageable pageable);
-
-    @Query("SELECT p FROM Post p JOIN p.postCategoryMatches pcm WHERE pcm.category.category = :categoryName")
-    Page<Post> findByCategoryName(@Param("categoryName") String categoryName, Pageable pageable);
 
     Page<Post> findAllByUserId(Long userId, Pageable pageable);
 
--- src/main/java/com/team9oogling/codyus/domain/post/repository/PostRepositoryImpl.java
+++ src/main/java/com/team9oogling/codyus/domain/post/repository/PostRepositoryImpl.java
@@ -5,7 +5,9 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.team9oogling.codyus.domain.post.entity.Post;
+import com.team9oogling.codyus.domain.post.entity.QCategory;
 import com.team9oogling.codyus.domain.post.entity.QPost;
+import com.team9oogling.codyus.domain.post.entity.QPostCategoryMatches;
 import com.team9oogling.codyus.domain.post.entity.SearchType;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -68,5 +70,31 @@ public class PostRepositoryImpl implements PostRepositoryCustom{
 
 
         return new PageImpl<>(posts, pageable, total);
+    }
+
+    @Override
+    public Page<Post> findByCategoryName(String categoryName, Pageable pageable) {
+        QPost post = QPost.post;
+        QPostCategoryMatches matches = QPostCategoryMatches.postCategoryMatches;
+        QCategory category = QCategory.category1;
+
+        List<Post> postIds  = queryFactory
+            .select(post)
+            .from(matches)
+            .join(matches.category, category)
+            .where(category.category.eq(categoryName))
+            .orderBy(post.createdAt.desc())
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+        Long total = queryFactory
+            .select(matches.post.count())
+            .from(matches)
+            .join(matches.category, category)
+            .where(category.category.eq(categoryName))
+            .fetchOne();
+
+        return new PageImpl<>(postIds, pageable, total != null ? total : 0);
     }
 }
